20cecc6abfda896dda53392d575e9df138cd0707
PettkoHarsanyi <pettko.harsanyi@gmail.com>
2023-06-10 19:07:38 +0200

    Enemies now shown and autoplay option

--- app/page.js
+++ app/page.js
@@ -48,6 +48,8 @@ export default function Home() {
     coordMap = coordMap.map(row => [...Array(map.width)]);
 
     map.treasures.forEach(treasure => { if (treasure.collectedByHeroId === null) coordMap[treasure.position.y][treasure.position.x] = "T" });
+    map.enemies.forEach(enemy => { if (enemy.health > 0) coordMap[enemy.position.y][enemy.position.x] = "E" });
+    map.bullets.forEach(bullet => { coordMap[bullet.position.y][bullet.position.x] = "B" });
 
     Object.entries(resource.compressedObstacles.coordinateMap).forEach(entry => {
       entry[1].forEach(y => {
@@ -77,7 +79,10 @@ export default function Home() {
     }
   }, [coordinateMap])
 
+  const [autoPlay, setAutoPlay] = useState(false);
+
   const move = () => {
+    if (!autoPlay) return;
     let obstacleMap = [...coordinateMap.map(y => y.map(x => x == "O" ? 0 : 1))]
     let graph = new Graph(obstacleMap)
     let start = graph.grid[hero.position.y][hero.position.x];
@@ -103,10 +108,10 @@ export default function Home() {
     })
 
     console.log(paths);
-    
+
     let flatPaths = []
 
-    paths.forEach(path=>flatPaths = flatPaths.concat(path))
+    paths.forEach(path => flatPaths = flatPaths.concat(path))
 
 
 
@@ -215,56 +220,54 @@ export default function Home() {
     })
   }
 
+  const attackUp = async () => {
+    await axios.post(BASE_URL + "/play/approveHeroTurn", {
+      "heroId": null,
+      "action": "KICK_DOWN"
+    }, { headers: { "story-playthrough-token": storyToken } }).then(async () => {
+      console.log("ÜTÖTTEM EGYET FEL")
+      setIsNextAttack(false)
+      await getState();
+    })
+  }
+
+  const attackDown = async () => {
+    await axios.post(BASE_URL + "/play/approveHeroTurn", {
+      "heroId": null,
+      // HIBALEHETŐSÉG, VAGY KICK_DOWN
+      "action": "KICK_UP"
+    }, { headers: { "story-playthrough-token": storyToken } }).then(async () => {
+      console.log("ÜTÖTTEM EGYET LE")
+      setIsNextAttack(false)
+      await getState();
+    })
+  }
+
+  const attackLeft = async () => {
+    await axios.post(BASE_URL + "/play/approveHeroTurn", {
+      "heroId": null,
+      "action": "KICK_LEFT"
+    }, { headers: { "story-playthrough-token": storyToken } }).then(async () => {
+      console.log("ÜTÖTTEM EGYET BALRA")
+      setIsNextAttack(false)
+      await getState();
+    })
+  }
+
+  const attackRight = async () => {
+    await axios.post(BASE_URL + "/play/approveHeroTurn", {
+      "heroId": null,
+      "action": "KICK_RIGHT"
+    }, { headers: { "story-playthrough-token": storyToken } }).then(async () => {
+      console.log("ÜTÖTTEM EGYET JOBBRA")
+      setIsNextAttack(false)
+      await getState();
+    })
+  }
+
+  const [isNextAttack, setIsNextAttack] = useState(false);
+
   return (
-    // <main className="flex min-h-screen flex-row items-center justify-center gap-10">
-    //   <div className="flex flex-col gap-10 ">
-    //     {storyToken &&
-    //       <div>
-    //         <div>story:</div>
-    //         {storyToken}
-    //       </div>
-    //     }
-    //     <div className='mainButton cursor-pointer text-center' onClick={() => startStory()}>
-    //       New Story
-    //     </div>
-    //     {storyToken &&
-    //       <div className="flex flex-col gap-3">
-    //         <div className="grid grid-cols-3">
-    //           <div className='moveButton cursor-pointer col-start-2' >
-    //             UP
-    //           </div>
-    //           <div>
-    //           </div>
-    //           <div className='moveButton cursor-pointer' onClick={() => moveLeft()}>
-    //             LEFT
-    //           </div>
-    //           <div className="moveButton cursor-pointer m-2" onClick={() => shield()}>
-    //             SHIELD
-    //           </div>
-    //           <div className='moveButton cursor-pointer col-start-3' onClick={() => moveRight()}>
-    //             RIGHT
-    //           </div>
-    //           <div className='moveButton cursor-pointer col-start-2' onClick={() => moveDown()}>
-    //             DOWN
-    //           </div>
-    //         </div>
-    //         <div className='mainButton cursor-pointer text-center' onClick={() => getState()}>
-    //           Get State
-    //         </div>
-    //       </div>
-    //     }
-    //   </div>
-    //   {storyToken &&
-    //     <div className="flex flex-col gap-5">
-    //       <div>Debugger:</div>
-    //       <table style={{ border: "0.5vh solid white", borderCollapse: "collapse" }}>
-    //         <tbody>
-    //           {coordinateMap && coordinateMap.map((row, index1) => { return <tr key={index1}>{row.map((cell, index2) => { return <td key={index2} className="w-10 h-10" style={{ verticalAlign: "center", textAlign: "center", border: "0.2vh solid white" }}>{coordinateMap[coordinateMap.length - index1 - 1][index2]}</td> })}</tr> })}
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   }
-    // </main>
     <main className="flex min-h-screen flex-col lg:flex-row items-center justify-center lg:mt-0 mt-16">
       <div className="w-full lg:w-1/2 h-max lg:h-screen flex flex-row justify-center items-center" >
         <div className="flex flex-col py-10 px-8 gap-5" style={{ border: "0.6vh solid white", width: "55%", aspectRatio: "0.62", borderRadius: "3vh 3vh 7vh 7vh" }}>
@@ -285,29 +288,35 @@ export default function Home() {
             <div className="flex flex-row w-full">
               <div className="w-1/2">
                 <div className="aspect-square relative left-[15%] grid grid-cols-3" style={{ width: "60%" }}>
-                  <div className="col-start-2 moveButton mBup" style={{ border: "solid white", borderWidth: "0.3vh 0.3vh 0 0.3vh", borderRadius: "0.5vh 0.5vh 0 0", zIndex: 52 }} onClick={() => coordinateMap ? moveUp() : null}><BiUpArrow /></div>
+                  <div className="col-start-2 moveButton mBup" style={{ border: "solid white", borderWidth: "0.3vh 0.3vh 0 0.3vh", borderRadius: "0.5vh 0.5vh 0 0", zIndex: 52 }} onClick={() => isNextAttack ? attackUp() : coordinateMap ? moveUp() : null}><BiUpArrow className={`${isNextAttack ? "text-red-700" : "text-white"}`} /></div>
                   <div></div>
-                  <div className="moveButton mBleft" style={{ border: "solid white", borderWidth: "0.3vh 0 0.3vh 0.3vh", borderRadius: "0.5vh 0 0 0.5vh" }} onClick={() => coordinateMap ? moveLeft() : null}><BiLeftArrow /></div>
-                  <div className="flex flex-col justify-center items-center" style={{ overflow: "hidden", backgroundColor: "black", zIndex: 100 }}>o</div>
-                  <div className="moveButton mBright" style={{ border: "solid white", borderWidth: "0.3vh 0.3vh 0.3vh 0", borderRadius: " 0 0.5vh 0.5vh 0" }} onClick={() => coordinateMap ? moveRight() : null}><BiRightArrow /></div>
-                  <div className="col-start-2 moveButton mBdown" style={{ border: "solid white", borderWidth: "0 0.3vh 0.3vh 0.3vh", borderRadius: "0 0 0.5vh 0.5vh" }} onClick={() => coordinateMap ? moveDown() : null}><BiDownArrow /></div>
+                  <div className="moveButton mBleft" style={{ border: "solid white", borderWidth: "0.3vh 0 0.3vh 0.3vh", borderRadius: "0.5vh 0 0 0.5vh" }} onClick={() => isNextAttack ? attackLeft() : coordinateMap ? moveLeft() : null}><BiLeftArrow className={`${isNextAttack ? "text-red-700" : "text-white"}`} /></div>
+                  <div className="flex flex-col justify-center items-center" style={{ overflow: "hidden", backgroundColor: "black", zIndex: 100, color: isNextAttack ? "red" : "white" }}><div className={`w-1/2 h-1/2 rounded-full ${isNextAttack ? "animateCircle" : ""}`} style={{ border: "0.1vh solid white" }}></div></div>
+                  <div className="moveButton mBright" style={{ border: "solid white", borderWidth: "0.3vh 0.3vh 0.3vh 0", borderRadius: " 0 0.5vh 0.5vh 0" }} onClick={() => isNextAttack ? attackRight() : coordinateMap ? moveRight() : null}><BiRightArrow className={`${isNextAttack ? "text-red-700" : "text-white"}`} /></div>
+                  <div className="col-start-2 moveButton mBdown" style={{ border: "solid white", borderWidth: "0 0.3vh 0.3vh 0.3vh", borderRadius: "0 0 0.5vh 0.5vh" }} onClick={() => isNextAttack ? attackDown() : coordinateMap ? moveDown() : null}><BiDownArrow className={`${isNextAttack ? "text-red-700" : "text-white"}`} /></div>
                 </div>
               </div>
               <div className="w-1/2 flex flex-row justify-end items-center gap-2 lg:gap-5 pr-[5%]">
                 <div className="flex flex-col justify-center w-min relative top-3">
-                  <div className="aspect-square rounded-full mBup w-full" style={{ border: "0.2vh solid white" }} ></div>
-                  <div className="text-[0.5rem] sm:text-sm lg:text-xs xl:text-base" onClick={() => shield()}>SHIELD</div>
+                  <div className="aspect-square rounded-full cursor-pointer attackButton w-full" style={{ border: "0.2vh solid white" }} onClick={() => coordinateMap ? shield() : null}></div>
+                  <div className="text-[0.5rem] sm:text-sm lg:text-xs xl:text-base" >SHIELD</div>
                 </div>
                 <div className="flex flex-col justify-center w-min relative bottom-3">
-                  <div className="aspect-square rounded-full mBup w-full" style={{ border: "0.2vh solid white" }} ></div>
+                  <div className={`aspect-square rounded-full cursor-pointer attackButton ${isNextAttack ? "attackSelected" : ""} w-full`} onClick={() => { setIsNextAttack((isNextAttack) => !isNextAttack) }} style={{ border: "0.2vh solid white" }} ></div>
                   <div className="text-[0.5rem] sm:text-sm lg:text-xs xl:text-base">ATTACK</div>
                 </div>
               </div>
             </div>
-            <div className="px-4 text-xl mt-10 flex flex-col justify-center items-center cursor-pointer" onClick={() => startStory()}>
-              <div style={{ width: "6vh", height: "2vh", border: "0.1vh solid white", borderRadius: "10vh", }}></div>
-              <div className="font-extrabold px-4 text-sm">START</div>
+            <div className="px-4 text-xl mt-10 flex flex-row justify-center items-center" >
+              <div className="flex flex-col items-center gap-1 cursor-pointer" onClick={() => {setAutoPlay(false); startStory()}}>
+                <div style={{ width: "6vh", height: "2vh", border: "0.1vh solid white", borderRadius: "10vh", }}></div>
+                <div className="font-extrabold px-4 text-sm">START</div>
+              </div>
+              <div className="flex flex-col items-center gap-1 cursor-pointer" onClick={() =>{setAutoPlay(true); startStory()}}>
+                <div style={{ width: "6vh", height: "2vh", border: "0.1vh solid white", borderRadius: "10vh", }}></div>
+                <div className="font-extrabold px-4 text-sm">AUTO</div>
 
+              </div>
             </div>
           </div>
         </div>
